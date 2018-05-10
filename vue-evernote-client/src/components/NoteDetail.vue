4d6cba795e74c2568dfd8b1421cd4e2ba20ca6d9
lvsz <lvsuzhen@zhenmeilideMacBook-Pro.local>
2018-05-10 22:04:05 +0800

    笔记详情页

--- vue-evernote-client/src/components/NoteDetail.vue
+++ vue-evernote-client/src/components/NoteDetail.vue
@@ -1,8 +1,27 @@
 <template>
   <div class="note-detail" id="detail">
-      <!-- <h1>{{ $route.query.notebookId }}</h1> -->
       <noteSideBar></noteSideBar>
-      <div class="text-main"></div>
+      <div class="text-main">
+          <div class="note-bar">
+              <div class="note-status">
+                  <span>创建日期：5小时前</span>
+                  <span>更新日期：5小时前</span>
+                  <span>已保存</span>
+              </div>
+              <div class="note-btn">
+                  <i class="iconfont icon-delete"></i>
+                  <i class="iconfont icon-eye"></i>
+                  <i class="iconfont icon-edit"></i> 
+              </div>
+          </div>
+          <div class="note-title">
+              <input type="text" :value="currNote.title" placeholder="输入标题">
+          </div>
+          <div class="editor">
+            <textarea name="" :value="currNote.content" v-show="true" placeholder="输入内容，支持markdown语法"></textarea>
+            <div class="preview markdown-body" v-show="false"></div>
+          </div>
+      </div>
   </div>
 </template>
 
@@ -36,8 +55,36 @@ import auth from '@/apis/auth'
     }
 </script>
 
-<style scoped>
-.text-main {background-color: #fff;}
+<style lang="less" scoped>
+@import url(//at.alicdn.com/t/font_661819_ea491u4a8xgvi.css);
+
+.note-detail {
+    display: flex;
+    flex: 1;
+    align-items: stretch;
+    background-color: #fff;
+}
+.text-main{
+    flex: 1;
+    flex-direction:column;
+}
+.note-bar {
+    display: flex;
+    padding: 5px 10px;
+    border-bottom: 1px solid #eee;
+    justify-content: space-between;
+}
+.note-status { 
+    font-size: 13px; 
+    span {
+        margin-right: 8px;
+        color: #999;
+    }
+}
+.note-btn{
+    i{ margin-right: 4px;fill:#eee}
+}
+
 </style>
 
 
